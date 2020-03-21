d9e0e062833a83685f3644f5a609cf7dc8b20978
Aleksei Arro <polsck@gmail.com>
2020-03-21 22:17:30 +0300

    Minor code formatting

--- src/hocs/withClassList.jsx
+++ src/hocs/withClassList.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import createHOC from '@/utils/create-hoc.js';
 
-export default createHOC('ClassList', (WrappedComponent, classList) => (
-  function WithClassList(props) {
-    return <WrappedComponent {...props} classList={classList} />;
-  }
-));
+export default createHOC(
+    'ClassList',
+    (WrappedComponent, classList) => (
+      function WithClassList(props) {
+        return <WrappedComponent {...props} classList={classList} />;
+      }
+    )
+);
