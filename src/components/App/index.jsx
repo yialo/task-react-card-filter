1ce853e6d3b40e7b51796c1104e6c29a400a9fc0
Aleksei Arro <polsck@gmail.com>
2020-03-22 14:53:48 +0300

    Redundant decorated 'Input' component file removed

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 
-import InputWithClassList from '../Input/WithClassList.jsx';
 import Card from '../Card/index.jsx';
+import Input from '../Input/index.jsx';
+
+import withClassList from '@/hocs/withClassList.jsx';
+
+const AppFilter = withClassList(Input, ['app__filter']);
 
 function App() {
   return (
     <div className="app">
       <div className="app__searchbar" role="search">
-        <InputWithClassList
+        <AppFilter
           label="Введите номер заявки"
           placeholder="Номер заявки"
           onChange={() => {
+            // STUB:
             console.log('Changed!');
           }}
         />
-        <InputWithClassList
+        <AppFilter
           label="Укажите наименование клиента"
           placeholder="Наименование клиента"
           onChange={() => {
+            // STUB:
             console.log('Changed!');
           }}
         />
--- src/components/Input/WithClassList.jsx
+++
@@ -1,5 +0,0 @@
-import Input from './index.jsx';
-
-import withClassList from '@/hocs/withClassList.jsx';
-
-export default withClassList(Input, ['app__filter']);
